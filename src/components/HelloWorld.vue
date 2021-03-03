627e3f0e14ea6bf04468b7fc6333d0720cb7193f
star <star@qq.com>
2021-03-03 16:52:19 +0800

    练习计算属性

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="getName">得到小名</button>
+    <p>姓: <input type="text" v-model="firstName"></p>
+    <p>名: {{lastName}}</p>
+    <p>姓名: {{fullName}}</p>
+    <p>小名: {{xiaoming}}</p>
   </div>
 </template>
 
@@ -35,6 +14,34 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    // fullName: function() {
+    //   return this.firstName + this.lastName
+    // }
+    fullName: {
+      get: function() {
+        return this.firstName + this.lastName
+      },
+      set: function(val) {
+        console.log(val)
+        var xin = val.substring(0, 1)
+        console.log(xin)
+        this.xiaoming = '小' + xin
+      }
+    }
+  },
+  data() {
+    return {
+      firstName: '',
+      lastName: '侠',
+      xiaoming: ''
+    }
+  },
+  methods: {
+    getName() {
+      this.fullName = '李四'
+    }
   }
 }
 </script>

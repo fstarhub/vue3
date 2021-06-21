c234d51aa50e59a1971cfb9f7aa0d203576d1a39
star <star@qq.com>
2021-06-21 23:50:20 +0800

    增加前置类型转化

---
+++ src/App3.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-main>
+      hahah
+    </el-main>
+  </el-container>
+</template>
+<script>
+export default {
+  name: 'App',
+  // data() {
+  //   return {
+  //   }
+  // }
+  
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>

d1b096c171e76a0efe4c89838af39dd2c2a6b60a
star <star@qq.com>
2021-06-28 18:16:52 +0800

    customRef学习

--- src/App10.vue
+++ src/App10.vue
@@ -2,20 +2,56 @@
  * @Description: 
  * @Autor: fengshuai
  * @Date: 2021-06-19 12:17:48
- * @LastEditors: 冯帅
- * @LastEditTime: 2021-06-22 23:17:43
+ * @LastEditors: fengshuai
+ * @LastEditTime: 2021-06-28 14:32:15
 -->
 <template>
   <div class="demo">
     <!-- <p>{{name}}</p> -->
     <p>{{state}}</p>
+    <ul>
+      <li v-for="item in state1" :key="item.id">{{item.name}}</li>
+    </ul>
     <button @click="myFn">按钮</button>
   </div>
 </template>
 
 <script>
-// import {ref,toRef} from 'vue'
-import {ref} from 'vue'
+
+import {customRef} from 'vue'
+function myRef(value) {
+  return customRef((track, trigger) => {
+
+    fetch(value)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data, '-------')
+        value = data
+        trigger()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    return {
+      get() {
+        track() // 告诉vue这个数据是需要追踪变化的
+        console.log('get', value)
+        // 注意点：不能再get方法中发送网络请求
+        // 渲染界面 -> 调用get -> 发送网络请求
+        // 保存数据 -> 更新界面 -> 调用get
+        return value
+      },
+      set(newValue) {
+        console.log('set', newValue)
+        value = newValue
+        trigger() // 告诉vue出发界面更新
+      }
+    }
+  })
+}
 export default {
   name: 'App',
   components: {},
@@ -25,16 +61,28 @@ export default {
    * customRef: 返回一个ref对象，可以显示的控制依赖追踪和触发响应
    */
   setup() {
-    let state = ref(19) // reactivve({value: 19})
-
+    // let state = ref(19) // reactivve({value: 19})
+    let state = myRef(19)
     function myFn() {
-
       console.log(state)
-      state.value = 20
-
+      state.value += 1
+      console.log(state)
     }
-    // return {name, age, myFn}
-    return {state, myFn}
+
+
+    // fetch('../public/data.json')
+    //   .then((res) => {
+    //     return res.json()
+    //   })
+    //   .then((data) => {
+    //     console.log(data, '-------')
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+
+      let state1 = myRef('../public/data.json')
+    return {state, myFn, state1}
   }
 }
 </script>

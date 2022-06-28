df06f40c5f8c3e2ef3957121a966f1932fd1dd9d
feng <shuai@qq.com>
2022-06-28 17:19:36 +0800

    初始化webpack环境

---
+++ myTemplateEngine/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    // 静态文件目录
+    contentBase: path.join(__dirname, 'www'),
+    // 不压缩
+    compress: false,
+    // 端口号
+    port: 8080,
+    // 虚拟打包路径，bundle.js文件没有真正生成
+    publicPath: '/xuni/'
+  }
+};

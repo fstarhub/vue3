7a2fef0214d9974f6483519ca8a8f4d5a5b5a46b
feng <shuai@qq.com>
2022-07-01 17:25:46 +0800

    截取字符，变量

---
+++ myTemplateEngine/src/Scanner.js
@@ -0,0 +1,39 @@
+// Scanner类
+export default class Scanner {
+  constructor(templateStr) {
+    console.log('我是Scanner', templateStr);
+
+    this.templateStr = templateStr
+    // 指针
+    this.pos = 0;
+    this.tail = templateStr
+  }
+
+  // 走过指定内容（{{}}），没有返回值，功能弱
+  scan(tag) {
+    if (this.tail.indexOf(tag) == 0) {
+      // 改变指针
+      this.pos += tag.length
+      // 改变尾巴
+      this.tail = this.templateStr.substr(this.pos)
+    }
+  }
+
+  // 让指针进行扫描，知道遇见指定内容结束，并且能够返回结束之前路过的内容
+  scanUtil(stopTag) {
+    // 记录pos的值
+    const pos_backUp = this.pos
+    // 尾巴开头不是stopTag时，说明没扫描到stopTag
+    while(this.tail.indexOf(stopTag) != 0 && this.eos()) {
+      this.pos++
+      this.tail = this.templateStr.substr(this.pos)
+    }
+
+    return this.templateStr.substring(pos_backUp, this.pos)
+  }
+
+  // 指针是否已经到头
+  eos() {
+    return this.pos <= this.templateStr.length
+  }
+}
--- myTemplateEngine/src/index.js
+++ myTemplateEngine/src/index.js
@@ -1,1 +1,25 @@
-console.log('eee');
+import Scanner from './Scanner'
+
+// 提供全局SSG_templaterEngine对象
+window.SSG_templaterEngine = {
+  render(template, data) {
+    // console.log('333');
+
+    // 实例化扫描器，构造时提供一个参数，这个参数就是模板字符串，
+    // 扫描器针对模板字符串工作的
+    var scanner = new Scanner(template)
+
+    var word
+    while(scanner.eos()) {
+      word = scanner.scanUtil('{{')
+      console.log(word);
+      scanner.scan('{{')
+      
+      word = scanner.scanUtil('}}')
+      console.log(word);
+      scanner.scan('}}')
+    }
+    
+  }
+
+}
